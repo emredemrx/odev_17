2ee6f9a50fc4bfcaead396f71facce7cbd8acc56
emred <femredemir699@gmail.com>
2024-07-25 22:08:17 +0300

    odev 17 geliştirmesi

--- src/main/java/example/Personel.java
+++ src/main/java/example/Personel.java
@@ -6,39 +6,6 @@ public class Personel {
     private String personelSoyadi;
     private String department;
 
-    // Getter ve Setter metodları
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPersonelAdi() {
-        return personelAdi;
-    }
-
-    public void setPersonelAdi(String personelAdi) {
-        this.personelAdi = personelAdi;
-    }
-
-    public String getPersonelSoyadi() {
-        return personelSoyadi;
-    }
-
-    public void setPersonelSoyadi(String personelSoyadi) {
-        this.personelSoyadi = personelSoyadi;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
     @Override
     public String toString() {
         return "Personel{" +
